97d0e5d7a110290458c50d76d67d868becafb312
Ahmed Ali <aappmlaed@gmail.com>
2023-01-05 17:10:52 +0300

    adding Fav Page

---
+++ lib/FavPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FavPage extends StatefulWidget {
+  const FavPage({super.key});
+
+  @override
+  State<FavPage> createState() => _FavPageState();
+}
+
+class _FavPageState extends State<FavPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
