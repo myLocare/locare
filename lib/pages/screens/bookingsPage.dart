07378ecf5f9b8784e1e924f026fe54d3de323278
Ahmed Ali <aappmlaed@gmail.com>
2023-01-06 17:50:17 +0300

    nothing

--- lib/pages/screens/bookingsPage.dart
+++ lib/pages/screens/bookingsPage.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class BookingsPage extends StatefulWidget {
   const BookingsPage({super.key});
